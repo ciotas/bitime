228af93a8b225c341505aa4e75f7eaf0664c226f
tongmeng <linzikristy@qq.com>
2020-01-29 06:04:09 +0000

    edit add_references table migrations

--- database/migrations/2020_01_10_203659_add_references.php
+++ database/migrations/2020_01_10_203659_add_references.php
@@ -22,6 +22,10 @@ class AddReferences extends Migration
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('topic_id')->references('id')->on('topics')->onDelete('cascade');
         });
+
+        Schema::table('tags', function(Blueprint $table) {
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
     }
 
     /**
@@ -39,6 +43,9 @@ class AddReferences extends Migration
         Schema::table('replies', function (Blueprint $table) {
             $table->dropForeign(['user_id']);
             $table->dropForeign(['topic_id']);
+        });
+        Schema::table('tags', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
         });
     }
 }
