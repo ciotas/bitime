fa6e59f8f4008aa8e44611cd849e31ecbd18462a
tongmeng <linzikristy@qq.com>
2020-02-27 09:20:21 +0000

    fix bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Binance\API;
 use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Support\Facades\Schema;
 use VIACreative\SudoSu\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -30,6 +31,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
 	{
+        Schema::defaultStringLength(191);
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
 		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
