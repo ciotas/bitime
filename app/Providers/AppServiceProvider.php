26295833b4f4515765ba4bc75ab381558e9b22f8
tongmeng <linzikristy@qq.com>
2020-02-27 10:00:35 +0000

    fix migration bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,7 +31,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
 	{
-        Schema::defaultStringLength(191);
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
 		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
